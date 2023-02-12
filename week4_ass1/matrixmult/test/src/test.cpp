4a66ed2ea1e232d132b3e5d4096bd6104970fffd
Jeffrey Allen <ja0081@uah.edu>
2023-02-12 12:27:30 -0600

    Added slides version of matrix mult

--- week4_ass1/matrixmult/test/src/test.cpp
+++ week4_ass1/matrixmult/test/src/test.cpp
@@ -12,39 +12,39 @@ using namespace std;
 int main (int argc, char ** argv) {
   
     // Set size for matrices
-    int N = 256;
+    int N = 2;
     int VEC_SIZE = N*N;
 
     // allocate vectors x and y_reference
     
-    vector<float> matrix1(VEC_SIZE, 1.0);
-    // vector<float> matrix1 {
-    //     1.0, 2.0, 3.0, 4.0,
-    //     5.0, 6.0, 7.0, 8.0, 
-    //     9.0, 10.0, 11.0, 12.0,
-    //     13.0, 14.0, 15.0, 16.0
-    // };
+    //vector<float> matrix1(VEC_SIZE, 1.0);
+    vector<float> matrix1 {
+        1.0, 2.0, 3.0, 4.0,
+        5.0, 6.0, 7.0, 8.0, 
+        9.0, 10.0, 11.0, 12.0,
+        13.0, 14.0, 15.0, 16.0
+    };
     
-    vector<float> matrix2(VEC_SIZE, 1.0);
-    // vector<float> matrix2 {
-    //     16.0, 15.0, 14.0, 13.0,
-    //     12.0, 11.0, 10.0, 9.0,
-    //     8.0, 7.0, 6.0, 5.0,
-    //     4.0, 3.0, 2.0, 1.0
-    // };
+    //vector<float> matrix2(VEC_SIZE, 1.0);
+    vector<float> matrix2 {
+        16.0, 15.0, 14.0, 13.0,
+        12.0, 11.0, 10.0, 9.0,
+        8.0, 7.0, 6.0, 5.0,
+        4.0, 3.0, 2.0, 1.0
+    };
     vector<float> matrix3(VEC_SIZE, 1.0);
 
     // Provide arbitrary time value for random seed
-    srand((unsigned) time(NULL));
+    // srand((unsigned) time(NULL));
 
-    // initialize the matrix1 and matrix2 to some arbitrary values
-    for (int i=0; i<N; i++){
-        for (int j=0; j<N; j++){
-            matrix1[i*N+j] = rand() % 10;
-            matrix2[i*N+j] = rand() % 10;
-            matrix3[i*N+j] = 0;
-        }
-    }
+    // // initialize the matrix1 and matrix2 to some arbitrary values
+    // for (int i=0; i<N; i++){
+    //     for (int j=0; j<N; j++){
+    //         matrix1[i*N+j] = rand() % 10;
+    //         matrix2[i*N+j] = rand() % 10;
+    //         matrix3[i*N+j] = 0;
+    //     }
+    // }
 
     // Print A
     // for (int i=0; i<N; i++){
@@ -100,10 +100,9 @@ int main (int argc, char ** argv) {
     );
 
     // execute our matrix multiplication
-    int numOfRuns = 100;
+    int numOfRuns = 5;
     double elapsedTime_ms = 0.0f;
     double total_elapsedTime_ms = 0.0f;
-    Timer timer;
 
     double numberOfFlops = 2 * VEC_SIZE;
     double flopRate = 0.0f;
@@ -112,6 +111,7 @@ int main (int argc, char ** argv) {
     double numberOfWrites = VEC_SIZE;
     
     for (int runCount = 0; runCount < numOfRuns; runCount++) {
+        Timer timer;
         timer.start();
         matrixMultiplication(dev_A, dev_B, dev_C, N);
         timer.stop();
@@ -119,23 +119,6 @@ int main (int argc, char ** argv) {
         // get elapsed time, estimated flops per second, and effective bandwidth
         elapsedTime_ms = timer.elapsedTime_ms();
         total_elapsedTime_ms += elapsedTime_ms;
-
-        totalFlopRate += numberOfFlops / (elapsedTime_ms / 1.0e3);
-
-        checkCudaErrors (
-            cudaMemcpy (
-                matrix3.data(),
-                dev_C,
-                byteSize_C,
-                cudaMemcpyDeviceToHost
-            )
-        );
-
-        // Display progress 
-        if (runCount % 10 == 0) {
-            cout << runCount << "/" << numOfRuns << "\r";
-            cout.flush();
-        }
     }
 
     double totalReads = 2 * VEC_SIZE * numOfRuns;
@@ -146,7 +129,11 @@ int main (int argc, char ** argv) {
     double avg_flopRate = totalNumberOfFlops / (total_elapsedTime_ms / 1.0e3);
 
     printf (
-    "\n\t- Avg Computational Rate:         %20.16e Gflops\n",
+    "\n\t- Avg Elapsed Time:             %20.16e Ms\n",
+        total_elapsedTime_ms / 1.0e3
+    );
+    printf (
+    "\t- Avg Computational Rate:         %20.16e Gflops\n",
         avg_flopRate / 1e9 
     );
     double avg_effectiveBandwidth_bitspersec =
@@ -157,6 +144,15 @@ int main (int argc, char ** argv) {
         avg_effectiveBandwidth_bitspersec / 1e9 
     );
 
+    // checkCudaErrors (
+    //     cudaMemcpy (
+    //         matrix3.data(),
+    //         dev_C,
+    //         byteSize_C,
+    //         cudaMemcpyDeviceToHost
+    //     )
+    // );
+
     // Print C
     // for (int i=0; i<N; i++){
     //     for (int j=0; j<N; j++){
@@ -166,17 +162,18 @@ int main (int argc, char ** argv) {
     // }
 
     // Now do the matrix multiplication on the CPU
-
     // vector<float> matrixCheck(VEC_SIZE);
 
-    // float sum;
-    // for (int row=0; row<N; row++){
-    //     for (int col=0; col<N; col++){
-    //         sum = 0;
-    //         for (int i=0; i<N; i++){
-    //             sum += matrix1[row*N+i]*matrix2[i*N+col];
+    // for (int i = 0; i < numOfRuns; i++){
+    //     float sum = 0.0;
+    //     for (int row=0; row<N; row++){
+    //         for (int col=0; col<N; col++){
+    //             sum = 0;
+    //             for (int i=0; i<N; i++){
+    //                 sum += matrix1[row*N+i]*matrix2[i*N+col];
+    //             }
+    //             matrixCheck[row*N+col] = sum;
     //         }
-    //         matrixCheck[row*N+col] = sum;
     //     }
     // }
 
@@ -187,8 +184,8 @@ int main (int argc, char ** argv) {
     //     }
     // }
 
-    // if (err) cout << "ERROR: The two matricies do not match!!!" << endl;
-    // else cout << "SUCCESS: Woo! The matricies match." << endl;
+    if (err) cout << "ERROR: The two matricies do not match!!!" << endl;
+    else cout << "SUCCESS: Woo! The matricies match." << endl;
 
     return 0;
 
