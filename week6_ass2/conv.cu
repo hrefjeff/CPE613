fc73f9db7d924a2dee1b068bc9f27f4f2d194190
Jeffrey Allen <2584585+hrefjeff@users.noreply.github.com>
2023-02-22 11:02:53 -0600

    Update and rename color2gray.cu to conv.cu

--- week6_ass2/conv.cu
+++ week6_ass2/conv.cu
@@ -38,6 +38,25 @@ void rgb2gray_kernel (
 
 }
 
+__global__ void conv_2D_basic_kernel (float* N, float* F, float *P, int r, int width, int height)
+{
+    int outCol = blockIdx.x*blockDim.x + threadIdx.x;
+    int outRow = blockIdx.y*blockDim.y + threadIdx.y;
+    
+    float Pvalue = 0.0f;
+    
+    for (int fRow = 0; fRow < 2*r+1; fRow++) {
+        for (int fCol = 0; fCol < 2*r+1; fCol++) {
+            inRow = outRow - r + fRow;
+            inCol = outCol - r + fCol;
+            if (inRow >= 0 && inRow < height && inCol >= 0 && inCol < width) {
+                Pvalue += F[fRow][fCol]*N[inRow*width + inCol];
+            }
+        }
+    }
+    P[outRow][outCol] = Pvalue;
+}
+
 void device_rgb2grayscale (
     unsigned char * deviceRed,
     unsigned char * deviceGreen,
