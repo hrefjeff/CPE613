d13ed92448cb5f5f9c2d26f14ace992bd27b4fc5
Jeffrey Allen <jeffwayne256@gmail.com>
2023-02-26 11:58:12 -0600

    Constant memory kernel

--- week6_ass2/conv.cu
+++ week6_ass2/conv.cu
@@ -12,65 +12,77 @@ using namespace std;
 // 2*r+1
 #define FILTER_SIZE 7
 // Allocate mask in constant memory
-__constant__ int FILTER[7 * 7];
+__constant__ float FILTER[7 * 7];
 
 __global__
 void conv_2D_basic_kernel (
-    unsigned char* N_r, unsigned char* N_g, unsigned char* N_b, float* F, unsigned char* P, int r, int width, int height
+    unsigned char* N_b,
+    unsigned char* N_g,
+    unsigned char* N_r,
+    float* F,
+    unsigned char* P,
+    int r,
+    int numCols,
+    int numRows
 ) {
     int outCol = blockIdx.x*blockDim.x + threadIdx.x;
     int outRow = blockIdx.y*blockDim.y + threadIdx.y;
-    int filter_radius = 2 * r + 1;
     
     float a_r = 0.0;
     float a_g = 0.0;
     float a_b = 0.0;
     
     // Iterate through the filter rows
-    for (int fRow = 0; fRow < filter_radius; fRow++) {
+    for (int fRow = 0; fRow < FILTER_SIZE; fRow++) {
         // Iterate through the filter columns
-        for (int fCol = 0; fCol < filter_radius; fCol++) {
+        for (int fCol = 0; fCol < FILTER_SIZE; fCol++) {
             int inputRow = outRow - r + fRow;
             int inputCol = outCol - r + fCol;
             // If we're within the height of the input matrix
-            if (inputRow >= 0 && inputRow < height) {
+            if (inputRow >= 0 && inputRow < numRows) {
                 // If we're within the width of the input matrix
-                if (inputCol >= 0 && inputCol < width) {
-                    a_r += F[fRow*FILTER_SIZE + fCol] * N_r[inputRow*width + inputCol];
-                    a_g += F[fRow*FILTER_SIZE + fCol] * N_g[inputRow*width + inputCol];
-                    a_b += F[fRow*FILTER_SIZE + fCol] * N_b[inputRow*width + inputCol];
+                if (inputCol >= 0 && inputCol < numCols) {
+                    a_b += F[fRow*FILTER_SIZE + fCol] * N_b[inputRow*numCols + inputCol];
+                    a_g += F[fRow*FILTER_SIZE + fCol] * N_g[inputRow*numCols + inputCol];
+                    a_r += F[fRow*FILTER_SIZE + fCol] * N_r[inputRow*numCols + inputCol];
                 }
             }
         }
     }
-    P[outRow*width+outCol] = (unsigned char)a_r + (unsigned char)a_g + (unsigned char)a_b;
+    P[outRow*numCols + outCol] = (unsigned char)(a_r + a_g + a_b);
 }
 
 __global__
 void conv_2D_const_mem_kernel (
-    float* inputMatrix,
-    float* outputMatrix,
+    unsigned char* N_b,
+    unsigned char* N_g,
+    unsigned char* N_r,
+    unsigned char* outputMatrix,
     int r,
-    int width,
-    int height
+    int numCols,
+    int numRows
 ) {
     int outCol = blockIdx.x*blockDim.x + threadIdx.x;
     int outRow = blockIdx.y*blockDim.y + threadIdx.y;
 
-    float tmp = 0.0f;
+    unsigned char a_r = 0.0;
+    unsigned char a_g = 0.0;
+    unsigned char a_b = 0.0;
     
-    for (int fRow = 0; fRow < 2*r+1; fRow++) {
-        for (int fCol = 0; fCol < 2*r+1; fCol++) {
-            int inRow = outRow - r + fRow;
-            int inCol = outCol - r + fCol;
-            if (inRow >= 0 && inRow < height) {
-                if (inCol >= 0 && inCol < width) {
-                    tmp += FILTER[fRow*width+fCol] * inputMatrix[inRow*width+ inCol];
+    for (int fRow = 0; fRow < FILTER_SIZE; fRow++) {
+        for (int fCol = 0; fCol < FILTER_SIZE; fCol++) {
+            int inputRow = outRow - r + fRow;
+            int inputCol = outCol - r + fCol;
+            if (inputRow >= 0 && inputRow < numRows) {
+                if (inputCol >= 0 && inputCol < numCols) {
+                    a_b += FILTER[fRow*FILTER_SIZE + fCol] * N_b[inputRow*numCols + inputCol];
+                    a_g += FILTER[fRow*FILTER_SIZE + fCol] * N_g[inputRow*numCols + inputCol];
+                    a_r += FILTER[fRow*FILTER_SIZE + fCol] * N_r[inputRow*numCols + inputCol];
                 }
             }
         }
     }
-    outputMatrix[outRow*width+outCol] = tmp;
+    outputMatrix[outRow*numCols + outCol] = (unsigned char)(a_b + a_g + a_b);
 }
 
 void convolution (
@@ -93,29 +105,31 @@ void convolution (
     );
 
     // Perform CUDA computations on deviceMatrix, Launch Kernel
-    conv_2D_basic_kernel<<<
+    // conv_2D_basic_kernel<<<
+    //     gridSize,
+    //     blockSize
+    // >>> (
+    //     red,
+    //     green,
+    //     blue,
+    //     filterMatrix,
+    //     outputMatrix,
+    //     radius,
+    //     numCols,
+    //     numRows
+    // );
+    conv_2D_const_mem_kernel<<<
         gridSize,
         blockSize
     >>> (
         red,
         green,
         blue,
-        filterMatrix,
         outputMatrix,
         radius,
         numCols,
         numRows
     );
-    // conv_2D_const_mem_kernel<<<
-    //     gridSize,
-    //     blockSize
-    // >>> (
-    //     inputMatrix,
-    //     outputMatrix,
-    //     radius,
-    //     numCols,
-    //     numRows
-    // );
 
     checkCudaErrors(
         cudaGetLastError()
@@ -126,32 +140,33 @@ int main() {
 
     // Initialize image we want to work with
     Mat img = imread("thethreeamigos.jpeg", IMREAD_COLOR);
-    int total = img.total(); // total === rows*cols
-
-    vector<unsigned char> h_red(img.rows * img.cols);
-    vector<unsigned char> h_green(img.rows * img.cols);
-    vector<unsigned char> h_blue(img.rows * img.cols);
-    vector<unsigned char> h_output(img.rows * img.cols);
+    int imgTotalSize = img.total(); // total === rows*cols
 
     // Allocate memory in host RAM
-    for (int rowIdx = 0; rowIdx < img.rows; ++rowIdx) {
-        for (int colIdx = 0; colIdx < img.cols; ++colIdx) {
-            auto & vec = img.at<Vec<uchar, 3>>(rowIdx, colIdx);
-            int offset = rowIdx * img.cols + colIdx;
-            h_red[offset] = vec[2];
-            h_green[offset] = vec[1];
-            h_blue[offset] = vec[0];
-        }
-    }
+    Mat bgr_channels[3];
+    split(img, bgr_channels);
+    vector<unsigned char> h_blue(bgr_channels[0].data, bgr_channels[0].data + bgr_channels[0].total());
+    vector<unsigned char> h_green(bgr_channels[1].data, bgr_channels[1].data + bgr_channels[1].total());
+    vector<unsigned char> h_red(bgr_channels[2].data, bgr_channels[2].data + bgr_channels[2].total());
+    
+    vector<unsigned char> h_output(img.rows * img.cols);
 
     // Create convolution filter
     vector<float> h_filter = {
-        0.11, 0.11, 0.11, 0.11, 0.11,
-        0.11, 0.11, 0.11, 0.11, 0.11,
-        0.11, 0.11, 0.11, 0.11, 0.11,
-        0.11, 0.11, 0.11, 0.11, 0.11,
-        0.11, 0.11, 0.11, 0.11, 0.11
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11,
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11,
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11,
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11,
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11,
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11,
+        0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11
     };
+
+    // vector<float> h_filter = {
+    //     0.11, 0.11, 0.11,
+    //     0.11, 0.11, 0.11,
+    //     0.11, 0.11, 0.11
+    // };
 
     // vector<float> h_filter = {
     //     1, 1, 1, 1, 1,
@@ -165,10 +180,10 @@ int main() {
     unsigned char * d_red = nullptr;
     unsigned char * d_green = nullptr;
     unsigned char * d_blue = nullptr;
-    float* d_filter = nullptr;
+    float * d_filter = nullptr;
     unsigned char * d_output = nullptr;
 
-    size_t matrixByteSize = total * sizeof(unsigned char);
+    size_t matrixByteSize = imgTotalSize * sizeof(unsigned char);
     size_t filterByteSize = FILTER_SIZE * sizeof(float);
 
     checkCudaErrors(cudaMalloc(&d_red, matrixByteSize));
@@ -187,21 +202,20 @@ int main() {
     checkCudaErrors(
         cudaMemcpy (d_blue, h_blue.data(), matrixByteSize, cudaMemcpyHostToDevice)
     );
-
     checkCudaErrors(
         cudaMemcpy (d_filter, h_filter.data(), filterByteSize, cudaMemcpyHostToDevice)
     );
     checkCudaErrors(
         cudaMemcpy (d_output, h_output.data(), matrixByteSize, cudaMemcpyHostToDevice)
     );
-    // checkCudaErrors(
-    //     cudaMemcpyToSymbol(FILTER, h_filter.data(), filterByteSize)
-    // );
+    checkCudaErrors(
+        cudaMemcpyToSymbol(FILTER, h_filter.data(), filterByteSize)
+    );
 
     convolution (
-        d_red,
-        d_green,
         d_blue,
+        d_green,
+        d_red,
         d_filter,
         d_output,
         FILTER_RADIUS,
@@ -215,16 +229,23 @@ int main() {
     );
 
     // Copy result from gray matrix into matlab OpenCV input array format
-    Mat opencv_output(img.rows, img.cols, CV_8UC1, Scalar(0));
+    Mat opencv_output(img.rows, img.cols, CV_8UC1);
     for (int rowIdx = 0; rowIdx < img.rows; ++rowIdx) {
         for (int colIdx = 0; colIdx < img.cols; ++colIdx) {
             int offset = rowIdx * img.cols + colIdx;
             opencv_output.at<uchar>(rowIdx, colIdx) = h_output[offset];
+            // opencv_output.at<uchar>(rowIdx, colIdx) = h_red[offset] + h_green[offset] + h_blue[offset];
+            // h_output[offset] = h_red[offset] + h_green[offset] + h_blue[offset];
         }
     }
 
+    copy(h_output.begin(), h_output.end(), opencv_output.data);
+
     // Write img to gray.jpg
-    imwrite("grayboiz.jpg", opencv_output);
+    //imwrite("grayboiz.jpg", opencv_output);
+    imshow("Original", img);
+    imshow("Reconstructed", opencv_output);
+    waitKey(0);
 
     // Free memory
     cudaFree(d_red);
