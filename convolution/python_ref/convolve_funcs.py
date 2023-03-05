e5d2150cfd5b5f2b4228939464937b720324f6da
Jeffrey Allen <jeffwayne256@gmail.com>
2023-03-05 09:50:35 -0600

    Added beginning of fft and more gold arr1 and arr2

--- convolution/python_ref/convolve_funcs.py
+++ convolution/python_ref/convolve_funcs.py
@@ -23,3 +23,45 @@ def convolve_time_domain(arr1, arr2, radius):
         result[idxInput] = sum
 
     return result
+
+def fft_v(x):
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0] # Get the number of elements in the array
+    if np.log2(N) % 1 > 0:
+        raise ValueError("must be a power of 2")
+
+    N_min = min(N, 2)
+    
+    n = np.arange(N_min)
+    k = n[:, None]
+    M = np.exp(-2j * np.pi * n * k / N_min) # Calculate the DFT matrix
+    X = np.dot(M, x.reshape((N_min, -1))) # Calculate the DFT
+    while X.shape[0] < N:
+            X_even = X[:, :int(X.shape[1] / 2)]
+            X_odd = X[:, int(X.shape[1] / 2):]
+            terms = np.exp(-1j * np.pi * np.arange(X.shape[0])
+                            / X.shape[0])[:, None]
+            X = np.vstack([X_even + terms * X_odd,
+                        X_even - terms * X_odd])
+    return X.ravel()
+
+def convolve_fast_fourier_transform(arr1, arr2):
+    """ Convolve two arrays using the fast fourier transform
+        The convolution in the time domain is equivalent to
+        multiplication in the frequency domain
+    """
+
+    # Create a new array to hold the result
+    result = [0] * len(arr1)
+
+    # Calculate the FFT of the input arrays
+    fft1 = fft_v(arr1)
+    fft2 = fft_v(arr2)
+
+    # Multiply the FFTs
+    fft_result = fft1 * fft2
+
+    # Calculate the inverse FFT
+    result = np.real(fft_v(fft_result))
+    
+    return result
--- convolution/python_ref/driver.py
+++ convolution/python_ref/driver.py
@@ -4,7 +4,7 @@
 Author      : Jeffrey Allen
 Class       : CS613, General Purpose GPU Programming
 Assignment  : Semester Project
-Description : Time domain convolution reference implementation
+Description : Reference implementations of convolution tequniques
 ================================================================='''
 
 ''' Import downloaded dependencies '''
@@ -15,9 +15,13 @@ from os import path, chdir
 from math import floor
 
 ''' Import my defined dependencies '''
-from convolve_funcs import convolve_time_domain
+from convolve_funcs import (
+    convolve_time_domain,
+    convolve_fast_fourier_transform 
+)
 from utils import *
 
+''' Get the show on the road '''
 def main():
 
     # Define paths to testing data
@@ -28,31 +32,38 @@ def main():
         '../test_data')
     )
     
+    # Define setup info for run
     TESTING = False
+    CONV_METHOD = "time" # "time" or "fft" or "overadd" or "oversave"
+    NUM_ELEMENTS = 8192 # 1024, 2048, 4096, 8192, 16384, 32768, 65536
 
     if TESTING:
         signal = [8,2,5,4,1,7,3]
         filter = [1,3,5,3,1]
         filename_result = "result_frombook.txt"
     else:
-        # Set up info for run
-        num_elements = 2048 # 1024, 2048, 4096, 8192, 16384, 32768, 65536
+        # Define paths for output data
         chdir(gold_test_dir)
-        filename_1 = "arr1_" + str(num_elements) + ".txt"
-        filename_2 = "arr2_" + str(num_elements) + ".txt"
-        filename_result = "result_" + str(num_elements) + ".txt"
+        filename_1 = "arr1_" + str(NUM_ELEMENTS) + ".txt"
+        filename_2 = "arr2_" + str(NUM_ELEMENTS) + ".txt"
+        filename_result = CONV_METHOD + "_result_" + str(NUM_ELEMENTS) + ".txt"
 
         # Create data set if it doesn't exist
         if not path.exists(filename_1) or not path.exists(filename_2):
-            create_random_data_set(filename_1,num_elements)
-            create_random_data_set(filename_2,num_elements)
+            create_random_data_set(filename_1, NUM_ELEMENTS)
+            create_random_data_set(filename_2, NUM_ELEMENTS)
+
         # Read data set
         signal = read_array_from_text_file(filename_1)
         filter = read_array_from_text_file(filename_2)
 
     # Convolve
-    r = floor(len(filter)/2)
-    result = convolve_time_domain(signal, filter, r)
+    match CONV_METHOD:
+        case "time":
+            r = floor(len(filter)/2)
+            result = convolve_time_domain(signal, filter, r)
+        case "fft":
+            result = convolve_fast_fourier_transform(signal, filter)
     
     # Save result to file
     chdir(test_dir)
