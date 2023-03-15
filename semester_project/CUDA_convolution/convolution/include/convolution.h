14787858be6ccd20a7fa0db5e2d02618f69effdf
Jeffrey Allen <jeffwayne256@gmail.com>
2023-03-14 21:35:08 -0500

    Added beginning folder structure of cuda fft. added time domain examples

---
+++ semester_project/CUDA_convolution/convolution/include/convolution.h
@@ -0,0 +1,10 @@
+#ifndef CONVOLUTION_CUH_
+#define CONVOLUTION_CUH_
+
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
+
+void convolution_1d_time();
+void convolution_1d_fft();
+
+#endif
---
+++ semester_project/CUDA_convolution/convolution/test/src/test.cpp
@@ -0,0 +1,14 @@
+/* Include my stuff */
+#include <convolution.h>
+#include <Timer.hpp>
+
+/* Include CUDA stuff */
+#include <cuda_runtime.h>
+#include <cufft.h>
+
+/* Include C++ stuff */
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
