e57d1eaf015af45a92d0ffea5372ac3f5b54c02e
Jeffrey Allen <jeffwayne256@gmail.com>
2023-04-21 15:06:53 -0500

    Updated CUDA conv 1d time to write to files

--- semester_project/CUDA_convolution/convolution/test/src/conv_1d_time.cu
+++ semester_project/CUDA_convolution/convolution/test/src/conv_1d_time.cu
@@ -12,14 +12,18 @@
         print result
 */
 
+/* Include C++ stuff */
 #include <iostream>
-#include <cmath>
+#include <string.h>
 #include <iostream>
+#include <complex>
 
 #include <convolution.h>
 
-#define N 8
-#define K 4
+#define N 1024
+#define K 1024
+
+using namespace std;
 
 int main() {
     float *h_input = new float[N];
@@ -30,34 +34,54 @@ int main() {
     cudaMalloc((void **)&d_filter, K * sizeof(float));
     cudaMalloc((void **)&d_output, (N + K - 1) * sizeof(float));
 
-    h_input[0] = 0.0;
-    h_input[1] = -1.0;
-    h_input[2] = -1.2;
-    h_input[3] = 2.0;
-    h_input[4] = 1.4;
-    h_input[5] = 1.4;
-    h_input[6] = 0.6;
-    h_input[7] = 0.0;
+    // Prepare to read signal and filter information from files
+    string signal_file_name =
+        "/home/jeff/code/CPE613/semester_project/test_data_gold/arr1_1024.txt";
+    string filter_file_name =
+        "/home/jeff/code/CPE613/semester_project/test_data_gold/arr2_1024.txt";
+    const char *output_file_name =
+        "/home/jeff/code/CPE613/semester_project/test_data/cuda_time_1024.txt";
 
-    h_filter[0] = 1.0;
-    h_filter[1] = -0.5;
-    h_filter[2] = -0.25;
-    h_filter[3] = -0.1;
+    ifstream signal_file(signal_file_name);
+    ifstream filter_file(filter_file_name);
+
+    if (signal_file.is_open()) {
+        int index = 0;
+        float value;
+        while (signal_file >> value) {
+            h_input[index++] = (float)(value);
+        }
+        signal_file.close();
+    } else {
+        std::cout << "Unable to open signal file." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (filter_file.is_open()) {
+        int index = 0;
+        float value;
+        while (filter_file >> value) {
+            h_filter[index++] = (float)(value);
+        }
+        filter_file.close();
+    } else {
+        std::cout << "Unable to open filter file." << std::endl;
+        return EXIT_FAILURE;
+    }
 
     int numOfThreads = 32;
     int numOfBlocks = (N + numOfThreads - 1) / numOfThreads;
 
     cudaMemcpy(d_input, h_input, N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_filter, h_filter, K * sizeof(float), cudaMemcpyHostToDevice);
-    convolve_1d<<<numOfBlocks, numOfThreads>>>(d_input, d_filter, d_output, N, K);
+    convolve_1d(d_input, d_filter, d_output, N, K);
     cudaMemcpy(h_output, d_output, (N + K - 1) * sizeof(float), cudaMemcpyDeviceToHost);
 
-    std::cout << std::endl;
+    FILE* filePtr = fopen(output_file_name, "w");
     for (int i = 0; i < N + K - 1; i++) {
-        printf ("%20.16e\n", h_output[i]);
+        fprintf (filePtr, "%20.16e\n", h_output[i]);
     }
-    std::cout << std::endl;
-
+    fclose(filePtr);
 
     delete[] h_input;
     delete[] h_filter;
