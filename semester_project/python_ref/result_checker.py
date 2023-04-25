ff68d5e497dddbdb74899f60404a42f85d2d5132
Jeffrey Allen <jeffwayne256@gmail.com>
2023-04-24 21:22:00 -0500

    Tested fft to 8192 elements

--- semester_project/python_ref/result_checker.py
+++ semester_project/python_ref/result_checker.py
@@ -4,8 +4,8 @@ from os import path, chdir
 import numpy as np
 from utils import read_array_from_text_file
 
-gold_file = 'time_result_1024.txt'
-result_file = 'cuda_time_1024.txt'
+gold_file = 'fft_result_8192.txt'
+result_file = 'cuda_fft_8192.txt'
 
 # Define paths to testing data
 gold_test_dir = path.abspath(path.join(path.dirname(__file__),
@@ -21,7 +21,7 @@ gold_test_data = read_array_from_text_file(gold_file)
 chdir(test_dir)
 test_data = read_array_from_text_file(result_file)
 
-if np.allclose(test_data, gold_test_data):
+if np.allclose(test_data, gold_test_data, 1e-02, 1e-02):
     print("Result is correct")
 else:
     print("Incorrect result")
