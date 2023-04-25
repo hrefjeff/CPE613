0c19b8f76c0cd3f6775e1514b5e78c280cefb292
Jeffrey Allen <jeffwayne256@gmail.com>
2023-04-25 12:48:30 -0500

    Code cleanup cuda 1d fft. Converted to in-place FFT. Added timer

--- semester_project/CUDA_convolution/convolution/test/src/test_fft.cu
+++ semester_project/CUDA_convolution/convolution/test/src/test_fft.cu
@@ -2,7 +2,6 @@
 
 https://developer.nvidia.com/blog/cuda-pro-tip-use-cufft-callbacks-custom-data-processing/
 
-
 */
 
 /* Include C++ stuff */
@@ -11,74 +10,93 @@ https://developer.nvidia.com/blog/cuda-pro-tip-use-cufft-callbacks-custom-data-p
 #include <cstdio>
 #include <cstdlib>
 
-/* Include my stuff */
-#include <convolution.h>
-#include <Timer.hpp>
-
 /* Include CUDA stuff */
 #include <cuda_runtime.h>
 #include <cufftXt.h>
 
-#define N 1024
-#define K 1024
+/* Include my stuff */
+#include <convolution.h>
+#include <Timer.hpp>
+
+#define N 8192
+#define K 8192
 #define BATCH_SIZE 1
 
 using namespace std;
 
 int main() {
-    cufftHandle plan1;
-    cufftHandle plan2;
+    cufftHandle plan;
     cudaStream_t stream = NULL;
 
     int FFT_SIZE = next_power_of_2(N + K - 1);
     
     bool file_status = false;
     string signal_file_name =
-        "/home/jeff/code/CPE613/semester_project/test_data_gold/arr1_1024.txt";
+        "/home/jeff/code/CPE613/semester_project/test_data_gold/arr1_8192.txt";
     string filter_file_name =
-        "/home/jeff/code/CPE613/semester_project/test_data_gold/arr2_1024.txt";
+        "/home/jeff/code/CPE613/semester_project/test_data_gold/arr2_8192.txt";
     const char *output_file_name =
-        "/home/jeff/code/CPE613/semester_project/test_data/cuda_fft_1024.txt";
+        "/home/jeff/code/CPE613/semester_project/test_data/cuda_fft_8192.txt";
 
     // Initialize the signal
+
+    // host signal initialization
     vector<cufftComplex> h_signal(FFT_SIZE, cufftComplex{0});
-    vector<cufftComplex> h_signal_fft(FFT_SIZE, cufftComplex{0});
     
     file_status = read_file_into_vector(signal_file_name, h_signal);
     if (file_status == false) return EXIT_FAILURE;
 
+    // device signal initialization
     cufftComplex *d_signal = nullptr;
-    cufftComplex *d_signal_fft = nullptr;
-    cudaMalloc(reinterpret_cast<void **>(&d_signal),
-                sizeof(cufftComplex) * h_signal.size());
-    cudaMalloc(reinterpret_cast<void **>(&d_signal_fft),
-                sizeof(cufftComplex) * h_signal_fft.size());
+    checkCudaErrors(
+        cudaMalloc(
+            reinterpret_cast<void **>(&d_signal),
+            sizeof(cufftComplex) * h_signal.size()
+        )
+    );
 
     // Initialize the filter
+
+    // host filter initialization
     vector<cufftComplex> h_filter(FFT_SIZE, cufftComplex{0});
-    vector<cufftComplex> h_filter_fft(FFT_SIZE, cufftComplex{0});
 
     file_status = read_file_into_vector(filter_file_name, h_filter);
     if (file_status == false) return EXIT_FAILURE;
-    
-    cufftComplex *d_filter = nullptr;
-    cufftComplex *d_filter_fft = nullptr;
-    cudaMalloc(reinterpret_cast<void **>(&d_filter),
-                sizeof(cufftComplex) * h_filter.size());
-    cudaMalloc(reinterpret_cast<void **>(&d_filter_fft),
-                sizeof(cufftComplex) * h_filter_fft.size());
 
-    // Initialize the product
+    // device filter initialization    
+    cufftComplex *d_filter = nullptr;
+    checkCudaErrors(
+        cudaMalloc(
+            reinterpret_cast<void **>(&d_filter),
+            sizeof(cufftComplex) * h_filter.size()
+        )
+    );
+
+    // Initialize the result
+
+    // host result initialization
     vector<cufftComplex> h_convolved_result(FFT_SIZE, cufftComplex{0});
     
+    // device result inintialization
     cufftComplex *d_convolved_fft = nullptr;
     cufftComplex *d_product_fft = nullptr;
 
-    cudaMalloc(reinterpret_cast<void **>(&d_convolved_fft),
-                sizeof(cufftComplex) * FFT_SIZE);
-    cudaMalloc(reinterpret_cast<void **>(&d_product_fft),
-                sizeof(cufftComplex) * FFT_SIZE);
+    checkCudaErrors(
+        cudaMalloc(
+            reinterpret_cast<void **>(&d_convolved_fft),
+            sizeof(cufftComplex) * h_convolved_result.size()
+        )
+    );
+    checkCudaErrors(
+        cudaMalloc(
+            reinterpret_cast<void **>(&d_product_fft),
+            sizeof(cufftComplex) * h_convolved_result.size()
+        )
+    );
 
+    // Copy host data to device
+    Timer timer;
+    timer.start();
     checkCudaErrors(
         cudaMemcpyAsync(
             d_signal, h_signal.data(),
@@ -97,49 +115,42 @@ int main() {
         )
     );
 
-    cufftCreate(&plan1);
-    cufftPlan1d(&plan1, FFT_SIZE, CUFFT_C2C, BATCH_SIZE);
-    //cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking);
-    //cufftSetStream(plan1, stream);
+    cufftCreate(&plan);
+    cufftPlan1d(&plan, FFT_SIZE, CUFFT_C2C, BATCH_SIZE);
 
     // Process signal    
-    cufftExecC2C(plan1, d_signal, d_signal_fft, CUFFT_FORWARD);
+    cufftExecC2C(plan, d_signal, d_signal, CUFFT_FORWARD);
 
     // Process filter
-    cufftExecC2C(plan1, d_filter, d_filter_fft, CUFFT_FORWARD);
+    cufftExecC2C(plan, d_filter, d_filter, CUFFT_FORWARD);
 
-    //cudaStreamSynchronize(stream); // force CPU thread to wait
-
-    checkCudaErrors(cudaGetLastError());
-
-    // dumpGPUDataToFile(d_signal_fft, {FFT_SIZE,1}, "cuda-fft-signal.txt");
-    // dumpGPUDataToFile(d_filter_fft, {FFT_SIZE,1}, "cuda-fft-filter.txt");
-
-    // Multiplication section
+    // Multiply the signals in their frequency form
     complexMulAndScaleGPU(
-        d_signal_fft,
-        d_filter_fft,
+        d_signal,
+        d_filter,
         d_product_fft,
         FFT_SIZE
     );
 
-    // dumpGPUDataToFile(d_product_fft, {FFT_SIZE,1}, "test.txt");
-
-    // Perform inverse
-    cufftCreate(&plan2);
-    cufftPlan1d(&plan2, FFT_SIZE, CUFFT_C2C, BATCH_SIZE);
-
     // Execute the inverse FFT on the result
-    cufftExecC2C(plan2, d_product_fft, d_convolved_fft, CUFFT_INVERSE);
+    cufftExecC2C(plan, d_product_fft, d_product_fft, CUFFT_INVERSE);
     
-    cudaMemcpyAsync(
-        h_convolved_result.data(), d_convolved_fft,
-        sizeof(cufftComplex) * FFT_SIZE,
-        cudaMemcpyDeviceToHost,
-        stream
+    checkCudaErrors(
+        cudaMemcpyAsync(
+            h_convolved_result.data(), d_product_fft,
+            sizeof(cufftComplex) * FFT_SIZE,
+            cudaMemcpyDeviceToHost,
+            stream
+        )
     );
+    timer.stop();
 
-    cudaStreamSynchronize(stream); // force CPU thread to wait
+    double elapsedTime_ms = timer.elapsedTime_ms();
+
+    printf (
+    "\n- Avg Elapsed Time:             %20.16e Ms\n\n",
+        elapsedTime_ms / 1.0e3
+    );
 
     FILE* filePtr = fopen(output_file_name, "w");
     float tmp;
@@ -151,17 +162,12 @@ int main() {
     
     /* free resources */
     cudaFree(d_signal);
-    cudaFree(d_signal_fft);
     cudaFree(d_filter);
-    cudaFree(d_filter_fft);
     cudaFree(d_product_fft);
     cudaFree(d_convolved_fft);
 
-    cufftDestroy(plan1);
-    cufftDestroy(plan2);
+    cufftDestroy(plan);
     cudaStreamDestroy(stream);
-
-    cudaDeviceReset();
 
     return EXIT_SUCCESS;
 }
